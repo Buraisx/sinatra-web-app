d7d783b035586314f010b6519d066135d9c492b9
Situ William <situ_william@yahoo.com>
2017-11-15 21:52:48 -0500

    finished adding redirects

--- server.rb
+++ server.rb
@@ -1,9 +1,26 @@
 require 'sinatra'
 
+get '/' do
+	redirect to('/home')
+end
 get	'/home' do
 	erb :index
 end
-
+get '/gallery' do
+	redirect to('/portfolio')
+end
 get '/portfolio' do
 	erb :gallery
+end
+
+get '/about' do
+	@skills = ['git','html','css','ruby']
+	@interests = ['cats','art','music','films','coffee']
+	erb :about
+end
+
+get '/favourites' do
+	@fav_links = ['//www.reddit.com', '//www.google.ca', '//www.netflix.ca', '//www.yahoo.ca', '//www.amazon.com']
+
+	erb :favourites	
 end
